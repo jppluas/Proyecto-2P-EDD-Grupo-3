defc9a8850ad6bccefe942dd6285e6ed8c3558df
Juan Pablo Plúas <107544861+jppluas@users.noreply.github.com>
2022-12-26 21:11:58 -0500

    update treemap

--- Grupo_03/src/main/java/grupo3/grupo_03/treemap/treemapg3.java
+++ Grupo_03/src/main/java/grupo3/grupo_03/treemap/treemapg3.java
@@ -1,43 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package grupo3.grupo_03.treemap;
 import java.io.File;
 import java.io.IOException;
-
-/**
- *
- * @author mbravop
- */
 public class treemapg3 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        String ruta="rutaxd";
-        File directoryPath = new File (ruta);
-        //List of all files and directories
-        File filesList[] = directoryPath.listFiles();
-        System.out.println("List of files and directories in directory:"+ ruta);
-        for(File file : filesList) {
-            if(file.isDirectory()){
-                File directory= file;
-                File filesList2[] = directory.listFiles();
-                System.out.println("List of files and directories in directory:"+file.getName());
-                for(File file2 : filesList2){
-                    System.out.println("File name: "+file2.getName());
-                    System.out.println("File path: "+file2.getAbsolutePath());
-                    System.out.println("Size :"+file2.length());
-                    System.out.println("");
-                }
-            }else{
+   public static void main(String args[]) throws IOException {
+      //Creating a File object for directory
+      String ruta="E:/Descargas/Heap";
+      getInfo(ruta);
+      
+   }
+   
+    public static void getInfo(String ruta){
+      File directoryPath = new File (ruta);
+      File filesList[] = directoryPath.listFiles();
+      System.out.println("List of files and directories in directory: "+ ruta);
+      for(File file : filesList) {
+        if(file.isDirectory()){
+            getInfo(file.getAbsolutePath());
+            
+        } else {
             System.out.println("File name: "+file.getName());
             System.out.println("File path: "+file.getAbsolutePath());
             System.out.println("Size :"+file.length());
             System.out.println("");
-            }
         }
+    
+    
+      }
     }
 }
