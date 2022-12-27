728e3809ff0b229d2c642875f87381d6688c065e
Juan Pablo Plúas <107544861+jppluas@users.noreply.github.com>
2022-12-26 20:29:35 -0500

    Primero

---
+++ Proyecto 2P EDD Grupo 3/src/main/java/com/mycompany/abc/Grupo_03.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.abc;
+
+import java.io.File;
+import java.io.IOException;
+public class Grupo_03 {
+   public static void main(String args[]) throws IOException {
+      //Creating a File object for directory
+      String ruta="E:/Descargas/Iterador";
+      File directoryPath = new File (ruta);
+      //List of all files and directories
+      File filesList[] = directoryPath.listFiles();
+      System.out.println("List of files and directories in directory:"+ ruta);
+      for(File file : filesList) {
+        if(file.isDirectory()){
+            File directory= file;
+            File filesList2[] = directory.listFiles();
+            System.out.println("List of files and directories in directory:"+file.getName());
+            for(File file2 : filesList2){
+                System.out.println("File name: "+file2.getName());
+                System.out.println("File path: "+file2.getAbsolutePath());
+                System.out.println("Size :"+file2.getTotalSpace());
+                System.out.println("");
+            }
+        } else{
+        System.out.println("File name: "+file.getName());
+        System.out.println("File path: "+file.getAbsolutePath());
+        System.out.println("Size :"+file.getTotalSpace());
+        System.out.println("");
+        }
+      }
+   }
+}
